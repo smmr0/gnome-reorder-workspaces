023d929e47b1790bfa83601aec82f4f66566fb59
Summer ☀️ <git@summerm.dev>
2022-10-02 15:00:44 -0600

    Refactor into class

--- src/extension.js
+++ src/extension.js
@@ -17,90 +17,91 @@ const { Gio, Meta, Shell } = imports.gi;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Main = imports.ui.main;
 
-let overviewConnections,
-	keybindings,
-	mutterSettings;
+class ReorderWorkspaces {
+	constructor() {
+		this.settings = ExtensionUtils.getSettings();
+		this.mutterSettings = new Gio.Settings({ schema_id: 'org.gnome.mutter' });
+		this.overviewConnections = {
+			'showing': { callback: this.enableKeybindings.bind(this) },
+			'hiding': { callback: this.disableKeybindings.bind(this) }
+		};
+		this.keybindings = {
+			'move-workspace-prev': { distance: -1 },
+			'move-workspace-next': { distance: 1 }
+		};
+	}
+
+	enable() {
+		this.connectToOverview();
+		if (Main.overview._visible) { this.enableKeybindings(); }
+	}
+
+	disable() {
+		this.disableKeybindings();
+		this.disconnectFromOverview();
+	}
+
+	connectToOverview() {
+		for (const overviewConnectionName in this.overviewConnections) {
+			const overviewConnection = this.overviewConnections[overviewConnectionName];
+
+			overviewConnection.id =
+				Main.overview.connect(overviewConnectionName, overviewConnection.callback);
+		}
+	}
+
+	disconnectFromOverview() {
+		this.overviewConnections
+			.map(oc => oc.id)
+			.filter(id => id !== undefined)
+			.forEach(id => Main.overview.disconnect(id));
+	}
+
+	enableKeybindings() {
+		for (const keybindingName in this.keybindings) {
+			const keybinding = this.keybindings[keybindingName];
+
+			Main.wm.addKeybinding(
+				keybindingName,
+				this.settings,
+				Meta.KeyBindingFlags.NONE,
+				Shell.ActionMode.OVERVIEW,
+				this.moveWorkspace.bind(this, keybinding.distance)
+			);
+		}
+	}
+
+	disableKeybindings() {
+		for (const keybindingName in this.keybindings) {
+			Main.wm.removeKeybinding(keybindingName);
+		}
+	}
+
+	moveWorkspace(distance) {
+		const activeWorkspace = global.workspace_manager.get_active_workspace();
+
+		if (this.workspaceIsEmptyDynamic(activeWorkspace)) {
+			return;
+		}
+
+		const currentIndex = activeWorkspace.index();
+		const newIndex = currentIndex + distance;
+		const workspaceAtNewIndex = global.workspace_manager.get_workspace_by_index(newIndex);
+
+		if (workspaceAtNewIndex === null || this.workspaceIsEmptyDynamic(workspaceAtNewIndex)) {
+			return;
+		}
+
+		global.workspace_manager.reorder_workspace(activeWorkspace, newIndex);
+	}
+
+	workspaceIsEmptyDynamic(workspace) {
+		return this.mutterSettings.get_boolean('dynamic-workspaces') &&
+			workspace.index() === global.workspace_manager.get_n_workspaces() - 1 &&
+			!workspace.list_windows().some(w => !w.on_all_workspaces);
+	}
+}
 
 function init() { // eslint-disable-line no-unused-vars
-	overviewConnections = {
-		'showing': { callback: enableKeybindings.bind(this) },
-		'hiding': { callback: disableKeybindings.bind(this) }
-	};
-	keybindings = {
-		'move-workspace-prev': { distance: -1 },
-		'move-workspace-next': { distance: 1 }
-	};
-	mutterSettings = new Gio.Settings({ schema_id: 'org.gnome.mutter' });
-}
-
-function enable() { // eslint-disable-line no-unused-vars
-	connectToOverview();
-	if (Main.overview._visible) { enableKeybindings(); }
-}
-
-function disable() { // eslint-disable-line no-unused-vars
-	disableKeybindings();
-	disconnectFromOverview();
-}
-
-function connectToOverview() {
-	for (const overviewConnectionName in overviewConnections) {
-		const overviewConnection = overviewConnections[overviewConnectionName];
-
-		overviewConnection.id =
-			Main.overview.connect(overviewConnectionName, overviewConnection.callback);
-	}
-}
-
-function disconnectFromOverview() {
-	overviewConnections
-		.map(oc => oc.id)
-		.filter(id => id !== undefined)
-		.forEach(id => Main.overview.disconnect(id));
-}
-
-function enableKeybindings() {
-	const settings = ExtensionUtils.getSettings();
-
-	for (const keybindingName in keybindings) {
-		const keybinding = keybindings[keybindingName];
-
-		Main.wm.addKeybinding(
-			keybindingName,
-			settings,
-			Meta.KeyBindingFlags.NONE,
-			Shell.ActionMode.OVERVIEW,
-			moveWorkspace.bind(this, keybinding.distance)
-		);
-	}
-}
-
-function disableKeybindings() {
-	for (const keybindingName in keybindings) {
-		Main.wm.removeKeybinding(keybindingName);
-	}
-}
-
-function moveWorkspace(distance) {
-	const activeWorkspace = global.workspace_manager.get_active_workspace();
-
-	if (workspaceIsEmptyDynamic(activeWorkspace)) {
-		return;
-	}
-
-	const currentIndex = activeWorkspace.index();
-	const newIndex = currentIndex + distance;
-	const workspaceAtNewIndex = global.workspace_manager.get_workspace_by_index(newIndex);
-
-	if (workspaceAtNewIndex === null || workspaceIsEmptyDynamic(workspaceAtNewIndex)) {
-		return;
-	}
-
-	global.workspace_manager.reorder_workspace(activeWorkspace, newIndex);
-}
-
-function workspaceIsEmptyDynamic(workspace) {
-	return mutterSettings.get_boolean('dynamic-workspaces') &&
-		workspace.index() === global.workspace_manager.get_n_workspaces() - 1 &&
-		!workspace.list_windows().some(w => !w.on_all_workspaces);
+	return new ReorderWorkspaces();
 }
